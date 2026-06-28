18f6cfeecb54184c4d29882b94b9076f0a412640
Valerius Petrini <valeriuspetrini@gmail.com>
2026-06-28 09:47:31 -0500

    Add: sqrtx - submission-15

---
+++ Data Structures & Algorithms/sqrtx/submission-15.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 1:
+            return x
+        left = 0
+        right = x // 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right
